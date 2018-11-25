3b224498daa78b82a43517aa019fa3ee74f106d9
Caitlin Wood <43452415+Caitlin-Wood@users.noreply.github.com>
2018-11-25 12:58:39 -0500

    src files for Components project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-import App from './components/App';
-import reducers from './reducers';
+const App = () => {
+	return (
+	  <div className="ui container comments">
+	  	<ApprovalCard>
+	  		<div>
+	  			<h4>Hello!</h4>
+					Are you sure you want to comment?
+				</div>
+	  	</ApprovalCard>
 
-const store = createStore (reducers, applyMiddleware(thunk))
+	  	<ApprovalCard>
+		  	<CommentDetail
+		      author="Sam" 
+		      timeAgo="Today at 5:00PM" 
+		      message="Nice blog post!" 
+		      profPic={faker.image.avatar()} 
+		    />
+	    </ApprovalCard>
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App /> 
-	</Provider>,
-	document.querySelector('#root')
-);
+	    <ApprovalCard>
+		  	<CommentDetail 
+		  	  author="Alex" 
+		  	  timeAgo="Today at 2:00PM" 
+		  	  message="Wow this is great" 
+		  	  profPic={faker.image.avatar()} 
+		  	/>
+	  	</ApprovalCard>
+
+	  	<ApprovalCard>
+		  	<CommentDetail 
+		  	  author="Jane" 
+		  	  timeAgo="Yesterday at 5:00PM" 
+		  	  message="i like what i see" 
+		  	  profPic={faker.image.avatar()} 
+		  	/>
+	  	</ApprovalCard>
+	  </div>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+//notes:    using {} to ref js object:
+//          ONE exception is Component within a component
+//          need to use < /> 
